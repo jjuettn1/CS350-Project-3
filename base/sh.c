7de275afd01348c01cbc0cb0a80fdc56965cf0e6
Jake Juettner <jjuettn1@cs.binghamton.edu>
2023-11-16 12:12:46 -0500

    Implented Background Functionality

--- base/sh.c
+++ base/sh.c
@@ -58,7 +58,7 @@ void
 runcmd(struct cmd *cmd)
 {
   //int p[2];
-  //struct backcmd *bcmd;
+  struct backcmd *bcmd;
   struct execcmd *ecmd;
   //struct listcmd *lcmd;
   //struct pipecmd *pcmd;
@@ -92,7 +92,11 @@ runcmd(struct cmd *cmd)
     break;
 
   case BACK:
-    printf(2, "Backgrounding not implemented\n");
+    //printf(2, "Backgrounding not implemented\n");
+    bcmd = (struct backcmd*)cmd;
+    if(fork1() == 0){
+      runcmd(bcmd->cmd);
+    }
     break;
   }
   exit();
